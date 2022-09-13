304735c318e4497660f2a0c33b84d5115ac67922
pavel <zzzzfogzzzz@gmail.com>
2022-09-13 15:54:29 +0600

    mistakes

---
+++ src/main/java/hw_1/Movable.java
@@ -0,0 +1,79 @@
+package hw_1;
+
+interface Movable {
+    void move();
+}
+
+interface Stoppable {
+    void stop();
+}
+
+class Engine {
+    void startEngine() {
+    }
+}
+
+abstract class Car {
+    private Engine engine;
+    private String color;
+    private String name;
+
+    protected void start() {
+        System.out.println("Car starting");
+    }
+
+    abstract void open();
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class LightWeightCar extends Car implements Movable {
+    @Override
+    void open() {
+        System.out.println("Car is open");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+}
+
+class Lorry extends Car implements Movable, Stoppable {
+    @Override
+    public void move() {
+        System.out.println("Car is moving");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car is stop");
+    }
+
+    @Override
+    void open() {
+    }
+}
+
