942b76705c687a95b213d46ea01bfae9be4f983c
pavel <zzzzfogzzzz@gmail.com>
2022-09-16 23:27:55 +0600

    arrayList

--- src/main/java/hw_2/Main.java
+++ src/main/java/hw_2/Main.java
@@ -2,12 +2,41 @@ package hw_2;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new SimpleLinkedList<>();
-        linkedList.insertFirst(1);
-        linkedList.insertFirst(2);
-        linkedList.insertFirst(3);
-        linkedList.insertFirst(4);
-        linkedList.insertFirst(5);
-        linkedList.display();
+        ArrayList<Integer> arrayList = new SimpleArrayList<>(0);
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(10);
+        arrayList.add(11);
+
+        for (Integer i : arrayList) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println(arrayList.get(2));
+
+        System.out.println(arrayList.contains(8));
+
+        arrayList.remove(8);
+
+        System.out.println(arrayList.contains(8));
+
+        System.out.println(arrayList);
+
+        arrayList.set(0, 1000000);
+
+        System.out.println(arrayList);
+
+        arrayList.clear();
+
+        System.out.println(arrayList);
     }
 }
